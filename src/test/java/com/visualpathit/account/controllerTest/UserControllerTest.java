23c9cd9647ae3905350afed569c8b17e7e9aad62
mafike <mafgenes1@icloud.com>
2024-11-03 13:09:46 -0500

    fail on mutaion line added

--- src/test/java/com/visualpathit/account/controllerTest/UserControllerTest.java
+++ src/test/java/com/visualpathit/account/controllerTest/UserControllerTest.java
@@ -1,7 +1,6 @@
 package com.visualpathit.account.controllerTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
